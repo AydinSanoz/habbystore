5178fea89f580fc0dbb51ed41c883ebaedcca002
E2029-Aydin <aydinsanoz2@gmail.com>
2021-04-11 19:30:21 +0300

    postData function added

---
+++ src/helper/postData.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const baseUrl = 'https://habby.store/wp-json/wc/v3';
+const consumer_key = 'ck_3cd96adeef6d7c7cea0945b987bc466eb607e5e2';
+const consumer_secret = 'cs_0aeaf5f789314fe3e8f9682c9af1d363d440d0bf';
+// const per_page = 90;
+// const parent = 0;
+
+export async function post(route, params) {
+  try {
+    const data = await axios.post(baseUrl + route, {
+      params: {
+        consumer_key: consumer_key,
+        consumer_secret: consumer_secret,
+        ...params,
+      },
+    });
+    return data;
+  } catch ({response}) {
+    if (response.status === 400) {
+      return {err: 'Aradığınız Ürün Bulunamadı'};
+    } else {
+      return {err: 'Network Error'};
+    }
+  }
+}

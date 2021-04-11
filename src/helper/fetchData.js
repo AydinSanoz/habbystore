88aac5da23d0e24589f3ac391a20ff04ee9a0250
E2029-Aydin <aydinsanoz2@gmail.com>
2021-04-11 14:59:10 +0300

    helper function  created

---
+++ src/helper/fetchData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const baseUrl = 'https://habby.store/wp-json/wc/v3';
+const consumer_key = 'ck_3cd96adeef6d7c7cea0945b987bc466eb607e5e2';
+const consumer_secret = 'cs_0aeaf5f789314fe3e8f9682c9af1d363d440d0bf';
+// const per_page = 90;
+// const parent = 0;
+
+export async function fetch(route, params) {
+  try {
+    const data = await axios.get(baseUrl + route, {
+      params: {
+        consumer_key: consumer_key,
+        consumer_secret: consumer_secret,
+        ...params,
+      },
+    });
+    return data;
+  } catch (error) {
+    return {'error occures': error};
+  }
+}

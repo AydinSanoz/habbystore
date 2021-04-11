2f97466cef74fba6861892abf4441463d7728820
E2029-Aydin <aydinsanoz2@gmail.com>
2021-04-11 15:02:04 +0300

    IconButton comp created

---
+++ src/components/IconButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export function IconButton(props) {
+  return (
+    <View style={{alignSelf: 'center'}}>
+      <Icon.Button
+        name={props.name}
+        size={30}
+        color="#000"
+        backgroundColor="transparent"
+        onPress={props.onPress}
+      />
+    </View>
+  );
+}

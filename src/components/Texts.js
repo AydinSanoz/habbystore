5fd1d906b784930bd68d473201aa464d9ccbf04b
E2029-Aydin <aydinsanoz2@gmail.com>
2021-04-13 22:43:40 +0300

    typography for texts added

---
+++ src/components/Texts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {texts, title} from './styles';
+
+export function Text1({children}) {
+  return <Text style={texts.text1}>{children}</Text>;
+}
+export function Text2({children}) {
+  return <Text style={texts.text2}>{children}</Text>;
+}

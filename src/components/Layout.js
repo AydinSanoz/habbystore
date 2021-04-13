def7a7dc1a4728dcc189e9a7890752dbaae44465
E2029-Aydin <aydinsanoz2@gmail.com>
2021-04-13 22:44:15 +0300

    refactoring done

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {View, Text, SafeAreaView, KeyboardAvoidingView} from 'react-native';
+import {View, SafeAreaView} from 'react-native';
 import {layout} from '../styles';
 
 export default function Layout(props) {
   return (
     <SafeAreaView style={layout.container}>
-      <View style={layout.container}>
-        <KeyboardAvoidingView>{props.children}</KeyboardAvoidingView>
-      </View>
+      <View style={layout.container}>{props.children}</View>
     </SafeAreaView>
   );
 }
--- src/helper/fetchData.js
+++ src/helper/fetchData.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 
-const baseUrl = 'https://habby.store/wp-json/wc/v3';
+const baseUrl = 'https://habby.store';
+const basePath = '/wp-json/wc/v3';
 const consumer_key = 'ck_3cd96adeef6d7c7cea0945b987bc466eb607e5e2';
 const consumer_secret = 'cs_0aeaf5f789314fe3e8f9682c9af1d363d440d0bf';
 // const per_page = 90;
 // const parent = 0;
 
+// const baseUrl = 'https://www.tekislastik.com/';
+// const consumer_key = 'ck_3cd96adeef6d7c7cea0945b987bc466eb607e5e2';
+// const consumer_secret = 'cs_0aeaf5f789314fe3e8f9682c9af1d363d440d0bf';
+
 export async function fetch(route, params) {
   try {
-    const data = await axios.get(baseUrl + route, {
+    const data = await axios.get(baseUrl + basePath + route, {
       params: {
         consumer_key: consumer_key,
         consumer_secret: consumer_secret,
